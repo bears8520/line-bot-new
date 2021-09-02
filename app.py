bce7be2655c2b54488c65fe8728c39df95b80ec6
Mike Chen <mikechen@gbd730.local>
2021-09-02 11:42:39 +0800

    update

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if ['熊哥', '貝卡'] in msg: #以下皆為訊息判別式
+    if '貝卡' in msg: #以下皆為訊息判別式
         msg = TextSendMessage(text='好帥')
     line_bot_api.reply_message(event.reply_token,msg)
 
